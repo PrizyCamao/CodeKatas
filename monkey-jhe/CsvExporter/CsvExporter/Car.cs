2e30c9b3536777d391615d303e44078aa7ab96ad
Joel Hertler <joel.hertler@camao.one>
2023-04-14 15:27:10 +0200

    csvExporterJhe

---
+++ monkey-jhe/CsvExporter/CsvExporter/Car.cs
@@ -0,0 +1,13 @@
+﻿namespace CsvExporter
+{
+    public class Car
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Car(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+    }
+}
---
+++ monkey-jhe/CsvExporter/CsvExporter/CsvExporterTests.cs
@@ -0,0 +1,49 @@
+﻿namespace CsvExporter
+{
+    public class CsvExporterTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            CsvExporter exporter = new CsvExporter();
+
+            var headers = Enumerable.Range(0, 10).Select(i => "Spalte" + i.ToString()).ToArray();
+            var rows = Enumerable.Range(0, 1000000).Select(row =>
+            Enumerable.Range(0, 10).Select(col => $"Row{row},Col{col}").ToArray()
+            ).ToArray();
+
+            var result = exporter.ExportToCsv(headers, rows);
+            File.WriteAllText("test1.csv", result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            CsvExporter exporter = new CsvExporter();
+
+            var cars = CreateCars().ToArray();
+
+            var result = exporter.ExportToCsv(cars);
+            File.WriteAllText("test2.csv", result);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            CsvExporter exporter = new CsvExporter();
+
+            var cars = CreateCars();
+
+            var result = exporter.ExportToCsv(cars);
+            File.WriteAllLines("test3.csv", result);
+        }
+
+        private IEnumerable<Car> CreateCars()
+        {
+            return Enumerable.Range(0, 1000000).Select(x => new Car(
+                    x++.ToString(),
+                    x.ToString()
+                ));
+        }
+    }
+}
