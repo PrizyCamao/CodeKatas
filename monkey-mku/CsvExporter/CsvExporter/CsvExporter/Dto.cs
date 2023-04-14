d88b79b3de5e0abed8307499e534b14f67e1720a
monkey-mku <mku@indeca.de>
2023-04-14 15:10:30 +0200

    Timetracker mku (#4)
    * add timetracker mku
    
    * CsvExporter Kata
    
    ---------
    
    Co-authored-by: Maximilian Kutter <maximilian.kutter@camao.one>

---
+++ monkey-mku/CsvExporter/CsvExporter/CsvExporter/Dto.cs
@@ -0,0 +1,16 @@
+﻿namespace CsvExporter
+{
+    public class Dto
+    {
+        public string Prop1 { get; set; }
+        public string Prop2 { get; set; }
+        public string Prop3 { get; set; }
+        public string Prop4 { get; set; }
+        public string Prop5 { get; set; }
+        public string Prop6 { get; set; }
+        public string Prop7 { get; set; }
+        public string Prop8 { get; set; }
+        public string Prop9 { get; set; }
+        public string Prop10 { get; set; }
+    }
+}
